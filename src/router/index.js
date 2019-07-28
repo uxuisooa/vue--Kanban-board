2f1139529fae5fecfe63d97d0cf6089d3473550a
uiuxsooa <sooa0513@naver.com>
2019-07-28 23:12:57 +0900

    동적 라우트 매칭

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
+import Board from '../components/Board.vue'
 import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter)
@@ -15,6 +16,7 @@ const router = new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/login', component: Login },
+    { path: '/b/:bid', component: Board }, //:bid 변수
     { path: '*', component: NotFound }
   ]
 })
