d913eae19a4c472f5e75627be057662f5d18742c
uiuxsooa <sooa0513@naver,com>
2019-07-29 23:43:00 +0900

    Ajax-HttpXMLRequst 객체

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,26 +1,53 @@
 <template>
+  <div>
+    Home
     <div>
-        Home
-        <div>
-            Board List : 
-            <ul>
-                <li>
-                    <router-link to="/b/1">Board 1</router-link>
-                </li>
-                <li>
-                    <router-link to="/b/2">Board 2</router-link>
-                </li>
-            </ul>
-        </div>
+      Board List :
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        Api result : {{ apiRes }}
+      </div>
+      <ul>
+        <li>
+          <router-link to="/b/1">Board 1</router-link>
+        </li>
+        <li>
+          <router-link to="/b/2">Board 2</router-link>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      loading: false,
+      apiRes: ''
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      const req = new XMLHttpRequest()
+      req.open('GET', 'http://localhost:3000/health')
+      req.send()
+      req.addEventListener('load', () => {
+        this.loading = false 
+        this.apiRes = {
+          status: req.status,
+          statusText: req.statusText,
+          response: JSON.parse(req.response)
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
