29a74eee497e4c77d20a3d2923ba12f1c4783db5
uiuxsooa <sooa0513@naver.com>
2019-07-30 00:24:52 +0900

    Axios 사용 및 에러확인하기

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,9 +4,9 @@
     <div>
       Board List :
       <div v-if="loading">Loading...</div>
-      <div v-else>
-        Api result : {{ apiRes }}
-      </div>
+      <div v-else>Api result : <pre>{{ apiRes }}</pre></div>
+      <!-- <div v-if="error"><pre>{{ error }}</pre></div> -->
+      <!-- 에러 확인 -->
       <ul>
         <li>
           <router-link to="/b/1">Board 1</router-link>
@@ -20,6 +20,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
@@ -33,17 +35,31 @@ export default {
   methods: {
     fetchData() {
       this.loading = true
-      const req = new XMLHttpRequest()
-      req.open('GET', 'http://localhost:3000/health')
-      req.send()
-      req.addEventListener('load', () => {
-        this.loading = false 
-        this.apiRes = {
-          status: req.status,
-          statusText: req.statusText,
-          response: JSON.parse(req.response)
-        }
-      })
+      // 에러 확인을 위한 주소 오타내기
+      // axios.get('http://localhost:3000/testerror')
+      axios.get('http://localhost:3000/health')
+        .then(res => {
+          this.apiRes = res.data
+        })
+        .catch(res => {
+          this.error = res.response.data
+        })
+        .finally(() => {
+          this.loading = false 
+        })
+
+      // const req = new XMLHttpRequest()
+      // req.open('GET', 'http://localhost:3000/health')
+      // req.send()
+      // req.addEventListener('load', () => {
+      //   this.loading = false 
+      //   this.apiRes = {
+      //     status: req.status,
+      //     statusText: req.statusText,
+      //     response: JSON.parse(req.response)
+      //   }
+      // })  
+
     }
   }
 }
