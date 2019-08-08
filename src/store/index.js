e7ecb7a973860707f6259d53a37cf40e333872ef
uiuxsooa <sooa0513@naver.com>
2019-08-08 11:59:26 +0900

    Vuex 적용 - 인증1 (token정보를 이용하여 로그인 정보를 확인. 로그인,로그아웃 변이 추가. 로그인 액션 추가. localStorage에서 토큰정보를 읽어와 로그인을 처리하는 로직 제작.

--- src/store/index.js
+++ src/store/index.js
@@ -7,14 +7,31 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     isAddBoard: false,
-    boards: []
+    boards: [],
+    token: null
+  },
+  getters: {
+    isAuth(state) {
+      return !!state.token
+     }
   },
   mutations: {
     SET_IS_ADD_BOARD (state, toggle) {
-      state.isAddBoard = toggle
+      state.isAddBoard  = toggle
     },
     SET_BOARDS(state, boards) { 
       state.boards = boards
+    },
+    LOGIN(state, token) { 
+      if (!token) return
+      state.token = token
+      localStorage.setItem('token', token)
+      api.setAuthInHeader(token)
+    },
+    LOGOUT(state, token) { 
+      state.token = null
+      delete localStorage.token
+      api.setAuthInHeader(null)
     }
   },
   actions: {
@@ -25,8 +42,15 @@ const store = new Vuex.Store({
       return api.board.fetch().then(data => {
         commit('SET_BOARDS', data.list)
        })
+    },
+    LOGIN({ commit }, { email, password }) {
+      return api.auth.login(email, password)
+        .then(({accessToken}) => commit('LOGIN', accessToken))
     }
   }
 })
+
+const { token } = localStorage
+store.commit('LOGIN', token)
 
 export default store
